5e2576def16ece84c72dabaf41c22cb8b7b0611f
willianfalbo <willianfalbo@gmail.com>
2020-08-12 00:11:14 -0300

    fetching data from api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,56 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-society-adidas-predator-19-3-tf/60/COL-7866-060/COL-7866-060_zoom2.jpg?ts=1573056930&ims=326x"
-          alt="Shoes"
-        />
-        <strong>Very nice shoes</strong>
-        <span>$129</span>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-          </div>
-          <span>Add to Cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-society-adidas-predator-19-3-tf/60/COL-7866-060/COL-7866-060_zoom2.jpg?ts=1573056930&ims=326x"
-          alt="Shoes"
-        />
-        <strong>Very nice shoes</strong>
-        <span>$129</span>
+  async componentDidMount() {
+    const response = await api.get('products');
+    const data = response.data.map((product) => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+    this.setState({ products: data });
+  }
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-          </div>
-          <span>Add to Cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-society-adidas-predator-19-3-tf/60/COL-7866-060/COL-7866-060_zoom2.jpg?ts=1573056930&ims=326x"
-          alt="Shoes"
-        />
-        <strong>Very nice shoes</strong>
-        <span>$129</span>
+  render() {
+    const { products } = this.state;
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-          </div>
-          <span>Add to Cart</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+    return (
+      <ProductList>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" />
+              </div>
+              <span>Add to Cart</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
