8ca77b51f87c3454e9b421b4a9751d8b8ef37e46
willianfalbo <willianfalbo@gmail.com>
2020-08-12 19:18:58 -0300

    removing products from cart (redux)

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -11,7 +11,13 @@ export default function cart(state = [], action) {
           draft.push({ ...action.product, amount: 1 });
         }
       });
-      return;
+    case 'REMOVE_FROM_CART':
+      return produce(state, (draft) => {
+        const productIndex = draft.findIndex((p) => p.id === action.id);
+        if (productIndex >= 0) {
+          draft.splice(productIndex, 1);
+        }
+      });
     default:
       return state;
   }
